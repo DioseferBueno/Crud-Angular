65f423234dba06261dace753c2d5b54b9edb3883
DioseferBueno <diosefer.ti@gmail.com>
2024-05-15 14:13:33 -0300

    conclusão das configurações do projeto, criando o primeiro serviço (ainda sem api)

---
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Course } from '../model/course';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  constructor(private httpClient: HttpClient) {}
+
+  private readonly API = '/assets/courses.json';
+
+  list(): Observable<Course[]> {
+    return this.httpClient.get<Course[]>(this.API).pipe();
+  }
+}
